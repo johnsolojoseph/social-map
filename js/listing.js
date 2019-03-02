e0bf038ca6eb5f0888111ed7989b5ff2fa41bd88
MichaelHastings <gameboy3787@gmail.com>
2019-03-02 12:58:38 -0800

    hey

--- js/listing.js
+++ js/listing.js
@@ -5,11 +5,16 @@ var firebase = firebase.database();
 
 firebase.ref().on("child_added", function(snapshot) {
   console.log(snapshot.val().title);
-
+  document.getElementById('listing').innerHTML += createCards(snapshot.val().title, snapshot.val().description);
 });
 
 
 
-function showListings(title, description) {
+function createCards(title, description) {
+  var card =""
   //Append to listing
+
+
+return card;
+
 }
