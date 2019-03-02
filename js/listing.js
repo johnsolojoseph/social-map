3f3404c04fda4e3c28cbe7d23a6e571c5f6e06f3
John Solo <johnsolojoseph@gmail.com>
2019-03-02 13:48:43 -0800

    Integrated Bitmoji to listings

--- js/listing.js
+++ js/listing.js
@@ -5,14 +5,20 @@ var firebase = firebase.database();
 
 firebase.ref().on("child_added", function(snapshot) {
   console.log(snapshot.val().title);
-  document.getElementById('listing').innerHTML += createCards(snapshot.val().title, snapshot.val().description);
+  document.getElementById('listing').innerHTML += createCards(snapshot.val().title, snapshot.val().description, snapshot.val().bitmoji);
 });
 
 
 
-function createCards(title, description) {
+function createCards(title, description, bitmoji) {
   if(description && title){
-  var card = "<div class='card-panel teal'><span class='white-text'>"+ title + "<br>" + description + " </span>  </div>"
+  var card =""
+  card = "<div class='card-panel teal'><span class='white-text'>"+ title + "<br>" + description + " </span>   "
+  if(bitmoji){
+  card += "<div class='center'><img src=" + "\"" + bitmoji + "\"" + "/></div> </div>"
+} else {
+  card += "</div>"
+}
   }
   else
   var card = "";
