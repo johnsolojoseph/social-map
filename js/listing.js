e936dcf08ffd9dc9e3f32672e0140641d5f8b936
John Solo <johnsolojoseph@gmail.com>
2019-03-02 16:15:45 -0800

    Restyled Post Listing

--- js/listing.js
+++ js/listing.js
@@ -13,7 +13,7 @@ firebase.ref().on("child_added", function(snapshot) {
 function createCards(title, description, bitmoji, dbKey) {
   if(description && title){
   var card =""
-  card = "<div class='card-panel card-list'><span class='white-text'>"+ title + "<br>" + description + " </span>   "
+  card = "<div class='card-panel card-list'><h3 class='white-text'>"+ title + "</h3><br><span class='white-text'>" + description + " </span> <br>  "
   if(bitmoji){
   card += "<div class='center'><img class='responsive-img' src=" + "\"" + bitmoji + "\"" + "/></div>"
   card += "<div id=" + "\"" + dbKey + "\"" +"></div></div>"
